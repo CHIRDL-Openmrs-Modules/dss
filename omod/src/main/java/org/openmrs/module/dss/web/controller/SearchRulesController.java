3aedeca2eff52dfb0387f2bf936537632f2a083f
sabhanda <sabhanda@140.182.66.200>
2022-09-02 12:47:59 -0400

    CHICA-1654 Velocity Vulnerability Fix

--- omod/src/main/java/org/openmrs/module/dss/web/controller/SearchRulesController.java
+++ omod/src/main/java/org/openmrs/module/dss/web/controller/SearchRulesController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.openmrs.api.context.Context;
